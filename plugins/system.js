3485f954efe740f886099fe3184f2bdb049711a6
joel james <162905644+joeljamestech2@users.noreply.github.com>
2024-11-19 18:22:56 +0300

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -12,18 +12,13 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let status = `*╭──────────●●►*
-*SILENT-SOBX-MD UPTIME LIST*
-
-*UPTIME:*  ${runtime(process.uptime())}
-
-*RAM USAGE:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-
-*HOSTNAME:* ${os.hostname()}
-
-*OWNER:* *SILENT LOVER⁴³²*
-*╰──────────●●►*
-`
+let status = `┏━❐
+┃*JOEL-MD UPTIME LIST*
+┃*UPTIME:*  ${runtime(process.uptime())}
+┃*RAM USAGE:* ${(process.memoryUsage().heapUsed / 1024 / ┃1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+┃*HOSTNAME:* ${os.hostname()}
+┃*OWNER:* *JOEL*
+┗━━❑`
 await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:`${status}`},{quoted:mek})
 
 }catch(e){

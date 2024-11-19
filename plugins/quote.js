864c613bd6916adddace23297d58dc273f68bb2d
joel james <162905644+joeljamestech2@users.noreply.github.com>
2024-11-19 18:55:35 +0300

    Update quote.js

--- plugins/quote.js
+++ plugins/quote.js
@@ -15,7 +15,7 @@ async (conn, mek, m, { from, reply }) => {
         const message = `
 💬 "${quote.content}"
 - ${quote.author}
-*QUOTES BY SILENTLOVER432*
+*QUOTES BY JOEL TECH*
         `;
         return reply(message);
     } catch (e) {

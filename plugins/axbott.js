d88721b15c4033f75c2b721bf4a22530b34d533e
joel james <162905644+joeljamestech2@users.noreply.github.com>
2024-11-21 21:11:09 +0300

    Create axbott.js

---
+++ plugins/axbott.js
@@ -0,0 +1,24 @@
+import config from '../../config.cjs';
+
+const ping = async (m, sock) => {
+  const prefix = config.PREFIX;
+const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+const text = m.body.slice(prefix.length + cmd.length).trim();
+
+  if (cmd === "bot") {
+    const start = new Date().getTime();
+    await m.React('⚡');
+    const end = new Date().getTime();
+    const responseTime = (end - start) / 1000;
+
+    const text = `┏۞*𝙹𝙾𝚎𝚕 𝚖𝚍 𝚋𝚘𝚝 𝚎𝚗𝚊𝚋𝚕𝚎𝚍*
+┃𝚔𝚎𝚎𝚙 𝚌𝚊𝚕𝚖 𝚍𝚞𝚍𝚎
+┗━━━━┓
+┏━━━━┛
+┃https://whatsapp.com/channel/0029Vade9VgD38CPEnxfYF0M
+┗━━━━━━━━━━━━━━━۞`;
+    sock.sendMessage(m.from, { text }, { quoted: m });
+  }
+}
+
+export default ping;
